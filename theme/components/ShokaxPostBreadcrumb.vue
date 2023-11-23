1b657604793359f1cb90ec580813b468a4fb1cfe
zkz098 <zkz20081204@126.com>
2023-11-23 11:25:17 +0800

    feat: 增加面包屑样式

--- theme/components/ShokaxPostBreadcrumb.vue
+++ theme/components/ShokaxPostBreadcrumb.vue
@@ -12,15 +12,15 @@ const blank = ref('')
 </script>
 
 <template v-if="typeof post.categories === 'object'">
-  <div class="breadcrumb" itemListElement itemscope itemtype="https://schema.org/BreadcrumbList">
-    <div class="i-ri-home-4-fill" />
-    <span>
+  <div class="breadcrumb text-sm inline-flex flex-wrap mt-5 mb-5 items-center" itemListElement itemscope itemtype="https://schema.org/BreadcrumbList">
+    <div class="i-ri-home-4-fill ml-0 mr-1.25" />
+    <span class="whitespace-pre">
       <RouterLink to="/">
         {{ t('menu.home') }}
       </RouterLink>
     </span>
   </div>
-  <div class="i-ri-arrow-right-s-line" />
+  <div class="i-ri-arrow-right-s-line ml-0.5 mr-0.5" />
   <span
     v-for="(item, index) in post.categories" :key="item" :class="{ current: index === Number(post.categories?.length) - 1 }"
     itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem"
@@ -31,6 +31,15 @@ const blank = ref('')
         <meta itemprop="position" :content="String(index)">
       </span>
     </RouterLink>
-    <div v-if="index > 1 && index !== Number(post.categories?.length) - 1" class="i-ri-arrow-right-s-line" />
+    <div v-if="index > 1 && index !== Number(post.categories?.length) - 1" class="i-ri-arrow-right-s-line  ml-0.5 mr-0.5" />
   </span>
 </template>
+
+<style scoped>
+.breadcrumb span.current {
+  background-color: var(--color-red-a1);
+  border-radius: .625rem;
+  padding: 0 .625rem;
+  transition: .2s ease-in-out;
+}
+</style>
